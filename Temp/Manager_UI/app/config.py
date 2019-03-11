aa27504bae9ed7fd5423d336d6b22815b5ba0db5
Hao Jin <chrisjin.h@gmail.com>
2019-03-11 00:25:36 -0400

    testing

---
+++ Temp/Manager_UI/app/config.py
@@ -0,0 +1,15 @@
+#db_config = {'user': 'master',
+#             'password': '96Gaowenhuan',
+#             'host': 'ece1779a2.c3quvy83lie8.us-east-1.rds.amazonaws.com',
+#             'database': 'ece1779'}
+
+
+
+db_config = {'user': 'master',
+             'password': 'ece1779pass',
+             'host': 'ece1779.c3z9wvey8adq.us-east-2.rds.amazonaws.com',
+             'database': 'a2'}
+             
+ami_id = 'ami-03a7ddeba320470a3'
+
+
