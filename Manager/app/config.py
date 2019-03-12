d63e30ee93ed37de5d8754863bbdb44f7eac5fd5
Louis Chen <louis.chen@mail.utoronto.ca>
2019-03-12 16:38:46 -0400

    update AMI

--- Manager/app/config.py
+++ Manager/app/config.py
@@ -3,7 +3,7 @@ db_config = {'user': 'master',
              'host': 'ece1779.c3z9wvey8adq.us-east-2.rds.amazonaws.com',
              'database': 'a2'}
              
-ami_id = 'ami-026261794e5955435'
+ami_id = 'ami-09af13d8385ef9965'
 
 target_group = 'arn:aws:elasticloadbalancing:us-east-1:560806999447:targetgroup/a2targetgroup/2f5dcca03fdf3575'
 
