329aa246e68af195456afacbc0510bbfe0a47161
Hao Jin <chrisjin.h@gmail.com>
2019-03-15 02:54:04 -0400

    change cpu check

--- Manager/cpu_check.py
+++ Manager/cpu_check.py
@@ -52,113 +52,115 @@ while True:
             load = point['Average']
             cpu_utilization.append(load)
 
-    average_cpu_utilization = sum(cpu_utilization) / len(cpu_utilization)
+    if len(cpu_utilization) != 0:
 
-    print("Cpu Utilization: "+ str(cpu_utilization))
-    print("Avg Cpu Utilization: "+str(average_cpu_utilization))
-    print("IDs: "+str(ids))
-    print("------------------------------------------")
+        average_cpu_utilization = sum(cpu_utilization) / len(cpu_utilization)
 
-    num_ins = len(ids)
-
-    if average_cpu_utilization > max_threshold:
-
-        num_ins_after_add =  num_ins * increase_ratio
-
-        num_of_ins_to_add = num_ins - num_ins_after_add
-
-        print("Condition: average_cpu_utilization > max_threshold")
-        print("Num of instances: "+str(num_ins))
-        print("Num of instances after expand: "+str(num_ins_after_add))
-        print("Increase Ratio: "+str(increase_ratio))
-        print("Num of instances to add: "+str(num_of_ins_to_add))
+        print("Cpu Utilization: "+ str(cpu_utilization))
+        print("Avg Cpu Utilization: "+str(average_cpu_utilization))
+        print("IDs: "+str(ids))
         print("------------------------------------------")
 
-        for i in range(num_of_ins_to_add):
-            print("Adding instance...")
-            ts = calendar.timegm(time.gmtime())
-            instances = ec2.create_instances(ImageId=config.ami_id,
-                                             InstanceType='t2.small',
-                                             MinCount=1,
-                                             MaxCount=1,
-                                             Monitoring={'Enabled': True},
-                                             Placement={'AvailabilityZone': 'us-east-1'},
-                                             SecurityGroups=[
-                                                 'ece1779',
-                                             ],
-                                             KeyName='ece1779',
-                                             TagSpecifications=[
-                                                 {
-                                                     'ResourceType': 'instance',
-                                                     'Tags': [
-                                                         {
-                                                             'Key': 'Group',
-                                                             'Value': 'User Instance'
-                                                         },
-                                                         {
-                                                             'Key': 'Name',
-                                                             'Value': str(ts)
-                                                         },
-                                                     ]
-                                                 },
-                                             ]
-                                             )
+        num_ins = len(ids)
 
-        for instance in instances:
-            print(instance.id)
-            ec2 = boto3.resource('ec2')
-            instance.wait_until_running(
-                Filters=[
-                    {
-                        'Name': 'instance-id',
-                        'Values': [
-                            instance.id,
-                        ]
-                    },
-                ],
-            )
+        if average_cpu_utilization > max_threshold:
 
-            print(instance.id)
-            client = boto3.client('elbv2')
-            client.register_targets(
-                TargetGroupArn=target_group,
-                Targets=[
-                    {
-                        'Id': instance.id,
-                    },
-                ]
-            )
+            num_ins_after_add =  num_ins * increase_ratio
 
-            waiter = client.get_waiter('target_in_service')
-            waiter.wait(
-                TargetGroupArn=target_group,
-                Targets=[
-                    {
-                        'Id': instance.id,
-                    },
-                ],
-            )
+            num_of_ins_to_add = num_ins - num_ins_after_add
 
-    if average_cpu_utilization < min_threshold:
+            print("Condition: average_cpu_utilization > max_threshold")
+            print("Num of instances: "+str(num_ins))
+            print("Num of instances after expand: "+str(num_ins_after_add))
+            print("Increase Ratio: "+str(increase_ratio))
+            print("Num of instances to add: "+str(num_of_ins_to_add))
+            print("------------------------------------------")
 
-        num_ins_after_shrink = int(num_ins / decrease_ratio)
+            for i in range(num_of_ins_to_add):
+                print("Adding instance...")
+                ts = calendar.timegm(time.gmtime())
+                instances = ec2.create_instances(ImageId=config.ami_id,
+                                                 InstanceType='t2.small',
+                                                 MinCount=1,
+                                                 MaxCount=1,
+                                                 Monitoring={'Enabled': True},
+                                                 Placement={'AvailabilityZone': 'us-east-1'},
+                                                 SecurityGroups=[
+                                                     'ece1779',
+                                                 ],
+                                                 KeyName='ece1779',
+                                                 TagSpecifications=[
+                                                     {
+                                                         'ResourceType': 'instance',
+                                                         'Tags': [
+                                                             {
+                                                                 'Key': 'Group',
+                                                                 'Value': 'User Instance'
+                                                             },
+                                                             {
+                                                                 'Key': 'Name',
+                                                                 'Value': str(ts)
+                                                             },
+                                                         ]
+                                                     },
+                                                 ]
+                                                 )
 
-        num_of_ins_to_remove = num_ins - num_ins_after_shrink
+            for instance in instances:
+                print(instance.id)
+                ec2 = boto3.resource('ec2')
+                instance.wait_until_running(
+                    Filters=[
+                        {
+                            'Name': 'instance-id',
+                            'Values': [
+                                instance.id,
+                            ]
+                        },
+                    ],
+                )
+
+                print(instance.id)
+                client = boto3.client('elbv2')
+                client.register_targets(
+                    TargetGroupArn=target_group,
+                    Targets=[
+                        {
+                            'Id': instance.id,
+                        },
+                    ]
+                )
+
+                waiter = client.get_waiter('target_in_service')
+                waiter.wait(
+                    TargetGroupArn=target_group,
+                    Targets=[
+                        {
+                            'Id': instance.id,
+                        },
+                    ],
+                )
+
+        if average_cpu_utilization < min_threshold:
+
+            num_ins_after_shrink = int(num_ins / decrease_ratio)
+
+            num_of_ins_to_remove = num_ins - num_ins_after_shrink
 
 
-        print("Condition: average_cpu_utilization < min_threshold")
-        print("Num of instances: "+str(num_ins))
-        print("Num of instances after shrink: "+str(num_ins_after_shrink))
-        print("Decrease Ratio: "+str(decrease_ratio))
-        print("Num of instances to remove: "+str(num_of_ins_to_remove))
-        print("------------------------------------------")
+            print("Condition: average_cpu_utilization < min_threshold")
+            print("Num of instances: "+str(num_ins))
+            print("Num of instances after shrink: "+str(num_ins_after_shrink))
+            print("Decrease Ratio: "+str(decrease_ratio))
+            print("Num of instances to remove: "+str(num_of_ins_to_remove))
+            print("------------------------------------------")
 
-        if num_of_ins_to_remove > 0 and num_ins_after_shrink > 0:
-            ids_to_remove = ids[:num_ins_after_shrink]
+            if num_of_ins_to_remove > 0 and num_ins_after_shrink > 0:
+                ids_to_remove = ids[:num_ins_after_shrink]
 
-            for id in ids_to_remove:
-                print("Removing instance...")
-                print(id)
-                ec2.instances.filter(InstanceIds=[id]).terminate()
+                for id in ids_to_remove:
+                    print("Removing instance...")
+                    print(id)
+                    ec2.instances.filter(InstanceIds=[id]).terminate()
 
     time.sleep(5)
