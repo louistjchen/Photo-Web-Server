2e300c57d02f76335d35631a65720ca8d29864b1
Louis Chen <louis.chen@mail.utoronto.ca>
2019-04-06 18:40:46 -0400

    rename folders

--- user/app/macro.py
+++
@@ -1,4 +0,0 @@
-secret_key = "chentuju&jinhao"
-username_char = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-password_char = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@#$%^&*_.?"
-salt_char = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
